c97e7e9cc47f22bd8f58ce6c1cbda7a7e14f5dc7
Qingyuan Tian <qingyuantian2006@gmail.com>
2024-11-08 00:24:15 -0500

    added page and userdescription component

---
+++ src/app/pages/profile/Userdescription.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Userdescription(props) {
+    return(
+        <div className='profile-page'>
+            <h1>{props.name}</h1>
+        </div>
+    )
+}
---
+++ src/app/pages/profile/page.tsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import Userdescription from './Userdescription'
+
+
+
+export default function Page() {
+    return(
+        <Userdescription />
+    )
+}
