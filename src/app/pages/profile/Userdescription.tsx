c6f5c1a894a67a4fd2792358e7d21533caf10401
Qingyuan Tian <qingyuantian2006@gmail.com>
2024-11-08 01:48:22 -0500

    added most of other types of elements on the page. no styling yet

--- src/app/pages/profile/Userdescription.tsx
+++ src/app/pages/profile/Userdescription.tsx
@@ -1,9 +1,53 @@
+import React from 'react';
 
 
-export default function Userdescription(props) {
-    return(
+interface User {
+    name: string;
+    username: string;
+    avatar: string;
+    bio: string;
+    school: string;
+    year: string;
+    gradStatus: string;
+    major: string;
+    minor?: string;
+    gpa: string;
+    coursesTaken: string[];
+    joinDate: string;
+    notesUploaded: number;
+    notesDownloaded: number;
+}
+
+interface UserDescriptionProps {
+    user: User;
+}
+
+export default function UserDescription({ user }: UserDescriptionProps): React.JSX.Element {
+    return (
         <div className='profile-page'>
-            <h1>{props.name}</h1>
+            <h1>{user.name}</h1>
+            <p>@{user.username}</p>
+            <p>Major: {user.major}</p>
+            {user.minor && <p>Minor: {user.minor}</p>}
+            <p>Joined {user.joinDate}</p>
+            <div className='description-notes'>
+                <ul>
+                    <li>Notes Uploaded {user.notesUploaded}</li>
+                    <li>Notes Downlaoded {user.notesDownloaded}</li>
+                    <li>Courses Taken {user.coursesTaken.length}</li>
+                </ul>
+            </div>
+            <div className="course-list">
+                <h2>Courses Taken</h2>
+                <ul>
+                    {user.coursesTaken.map((course, index) => (
+                        <li key={index}>{course}</li>
+                    ))}
+                </ul>
+            </div>
+            <div className="notes-gallery">
+                
+            </div>
         </div>
-    )
+    );
 }
--- src/app/pages/profile/page.tsx
+++ src/app/pages/profile/page.tsx
@@ -1,10 +1,40 @@
-import React, {useState} from 'react'
-import Userdescription from './Userdescription'
+// page.tsx
+import React from 'react';
+import UserDescription from './UserDescription';
+import HeaderCard from '@/app/components/headercard/HeaderCard';
 
 
+// Static example user data to be passed as a prop
+const user: User = {
+    name: "Alex Johnson",
+    username: "alex_j",
+    avatar: "/placeholder.svg?height=100&width=100",
+    bio: "Computer Science enthusiast | Note-taking aficionado",
+    school: "Stanford University",
+    year: "Junior",
+    gradStatus: "Undergraduate",
+    major: "Computer Science",
+    minor: "Data Science",
+    gpa: "3.8",
+    coursesTaken: [
+        "CS101: Introduction to Computer Science",
+        "CS201: Data Structures and Algorithms",
+        "CS301: Database Systems",
+        "MATH101: Calculus I",
+        "MATH201: Linear Algebra",
+        "DS101: Introduction to Data Science",
+        "DS201: Machine Learning Fundamentals"
+    ],
+    joinDate: "September 2022",
+    notesUploaded: 12,
+    notesDownloaded: 87,
+};
 
-export default function Page() {
-    return(
-        <Userdescription />
-    )
+export default function Page(): React.JSX.Element {
+    return (
+        <>
+            <HeaderCard />
+            <UserDescription user={user} />
+        </>
+    );
 }
